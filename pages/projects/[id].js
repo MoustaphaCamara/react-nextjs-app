717d2f0bad07005309de1acd37ecbc6e1d38983b
Mouss <104678778+MoussCarter@users.noreply.github.com>
2022-11-30 10:23:50 +0100

    v3

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,13 +1,30 @@
-const project = () => {
-  return <div></div>;
+import Image from "next/image";
+import { path } from "../../config";
+import { projects } from "../../projectsData";
+
+const project = ({ project }) => {
+  return (
+    <div className="project">
+      <h1>{project.title}</h1>
+      <span>{project.date}</span>
+      <p>{project.infos}</p>
+      <img
+        src={`../../public/img/projet-${project.id}.jpg`}
+        alt={project.title}
+      />
+      {/* <Image
+        src={path + project.img}
+        alt={project.title}
+        width={400}
+        height={250}
+      /> */}
+    </div>
+  );
 };
 
 export default project;
 
-export const getStaticProps = async () => {
-  await fetch();
-};
-export const getStaticPaths = (context) => {
+export const getStaticProps = (context) => {
   const filtered = projects.filter(
     (project) => project.id == context.params.id
   );
@@ -19,4 +36,14 @@ export const getStaticPaths = (context) => {
       },
     };
   }
+};
+
+export const getStaticPaths = async () => {
+  const ids = projects.map((project) => project.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 };
